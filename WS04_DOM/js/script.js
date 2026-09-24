064a21066365fcaf6c8487cd703b6e1686a5dee3
Petteri <Petteri.Elf@student.laurea.fi>
2026-09-24 14:14:11 +0300

    Workshop 4:n aloitus.

---
+++ WS04_DOM/js/script.js
@@ -0,0 +1,26 @@
+// Muokataan otsikkoa, kun nappia painetaan.
+
+const taskOneHeading = document.querySelector("#taskOneHeading");
+const changeHeadingButton = document.querySelector("#changeHeadingButton");
+
+changeHeadingButton.addEventListener("click", function () {
+    taskOneHeading.textContent = "Muokattu otsikko!";
+});
+
+
+
+// -------------------------------------------------- EXAMPLE 1 ANIMAL TABLE
+// -------------------------------------------------- EXAMPLE 1 ANIMAL TABLE
+
+
+
+// -------------------------------------------------- EXAMPLE 3 LISTEN DROPDOWN SELECT
+// -------------------------------------------------- EXAMPLE 3 LISTEN DROPDOWN SELECT
+
+
+
+// listener for the select element from the drop down list.
+
+
+
+    // function to update the DOM based on the selected animal
